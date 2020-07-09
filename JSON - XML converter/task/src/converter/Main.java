d58503a8a6a5117b3e608d7ce99dde035004465f
Dzmitry Marudau <vinmaster@tut.by>
2020-07-09 19:38:40 +0300

    Work on project. Stage 1/6: Elementary!

--- JSON - XML converter/task/src/converter/Main.java
+++ JSON - XML converter/task/src/converter/Main.java
@@ -1,8 +1,6 @@
 package converter;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,23 +22,34 @@ public class Main {
 
         try (Scanner scanner = new Scanner(System.in)) {
             String input = scanner.nextLine();
-            System.out.println(convert(input));
+            convert(input);
         }
     }
 
-    private static String convert(String input) {
+    private static void convert(String input) {
         if (input.startsWith("<")) {
             //XML -> JSON
             //Extract an element between tags and enclose it within double quotes.
-            //Extract element content and enclose it within double quotes. If element is empty, assign null - JSON value.
+            //Extract element content and enclose it within double quotes. If element is empty, assign null - JSON
+            // value.
+            int iFrom = input.indexOf(">");
+            int iTo = input.lastIndexOf("<");
+            String tag = input.substring(1, iFrom);
+            String content = input.substring(iFrom + 1, iTo);
+            System.out.printf("{\"%s\":\"%s\"}", tag, content);
         }
         if (input.startsWith("{")) {
             //JSON -> XML
             //Extract key between double quotes - XML element.
             //Extract element content from JSON value.
             //If value is not null, enclose JSON with <></>, else enclose within < />.
-        }
 
-        return input;
+            //cut the useless brackets
+            input = input.trim().substring(input.indexOf("\"") + 1, input.length() - 1); //{"jdk" : "1.8.9"} -> jdk":"1.8.9
+
+            String tag = input.substring(0, input.indexOf("\""));
+            String content = input.substring(input.indexOf(":") + 3);
+            System.out.printf("<%s>%s</%s>", tag, content, tag);
+        }
     }
 }
