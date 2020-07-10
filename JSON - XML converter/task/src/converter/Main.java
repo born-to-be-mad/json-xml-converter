500f962ce7198ced9d92f469427cea6cfff2df3e
Dzmitry Marudau <vinmaster@tut.by>
2020-07-10 16:36:08 +0300

    Work on project. Stage 1/6: Elementary

--- JSON - XML converter/task/src/converter/Main.java
+++ JSON - XML converter/task/src/converter/Main.java
@@ -32,11 +32,22 @@ public class Main {
             //Extract an element between tags and enclose it within double quotes.
             //Extract element content and enclose it within double quotes. If element is empty, assign null - JSON
             // value.
+            // <host>127.0.0.1</host>  -> {"host":"127.0.0.1"}
+            // <success/> -> {"success": null }
             int iFrom = input.indexOf(">");
-            int iTo = input.lastIndexOf("<");
-            String tag = input.substring(1, iFrom);
-            String content = input.substring(iFrom + 1, iTo);
-            System.out.printf("{\"%s\":\"%s\"}", tag, content);
+            int iTo = input.lastIndexOf("</");
+            String tag;
+            String content;
+            if (iTo == -1) {
+                tag = input.substring(input.indexOf("<") + 1, input.lastIndexOf("/>"));
+                content = null;
+                System.out.printf("{\"%s\": %s}", tag, content);
+            } else {
+                tag = input.substring(1, iFrom);
+                content = input.substring(iFrom + 1, iTo);
+                System.out.printf("{\"%s\":\"%s\"}", tag, content);
+            }
+
         }
         if (input.startsWith("{")) {
             //JSON -> XML
@@ -44,12 +55,20 @@ public class Main {
             //Extract element content from JSON value.
             //If value is not null, enclose JSON with <></>, else enclose within < />.
 
-            //cut the useless brackets
-            input = input.trim().substring(input.indexOf("\"") + 1, input.length() - 1); //{"jdk" : "1.8.9"} -> jdk":"1.8.9
-
-            String tag = input.substring(0, input.indexOf("\""));
-            String content = input.substring(input.indexOf(":") + 3);
-            System.out.printf("<%s>%s</%s>", tag, content, tag);
+            //{"success": null } -> <storage/>
+            //{"jdk" : "1.8.9"} -> <jdk>1.8.9</jdk>
+            // {"pizza":"slice"}
+            int t1 = input.indexOf("\"");
+            int t2 = input.indexOf("\"", t1 + 1);
+            String tag = input.substring(t1 + 1, t2);
+            int c1 = input.indexOf("\"", t2 + 1);
+            if (c1 == -1) {
+                System.out.printf("<%s/>", tag);
+            } else {
+                int c2 = input.indexOf("\"", c1 + 1);
+                String content = input.substring(c1 + 1, c2);
+                System.out.printf("<%s>%s</%s>", tag, content, tag);
+            }
         }
     }
 }
