5c38758aa42b4e818aebab23da292c80b6f5ea0c
Dzmitry Marudau <vinmaster@tut.by>
2020-07-21 16:10:04 +0300

    Solve tasks: - Getters and setters  Complex

---
+++ Problems/Complex/src/Main.java
@@ -0,0 +1,23 @@
+class Complex {
+
+    private double real;
+    private double imaginary;
+
+    public double getReal() {
+        return real;
+    }
+
+    public Complex setReal(double real) {
+        this.real = real;
+        return this;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public Complex setImaginary(double imaginary) {
+        this.imaginary = imaginary;
+        return this;
+    }
+}
