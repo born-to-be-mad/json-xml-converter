392b244f274dd80d6cb1a9972ceb6d5c3dd5f037
Dzmitry Marudau <vinmaster@tut.by>
2020-08-02 16:37:34 +0300

    Solve tasks: - Getting the fullname

---
+++ Problems/Getting the fullname/src/Main.java
@@ -0,0 +1,30 @@
+class User {
+    private String firstName;
+    private String lastName;
+
+    public User() {
+        this.firstName = "";
+        this.lastName = "";
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName == null ? "" : firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName == null ? "" : lastName;
+    }
+
+    public String getFullName() {
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            return "Unknown";
+        }
+        if (firstName.isEmpty()) {
+            return lastName;
+        }
+        if (lastName.isEmpty()) {
+            return firstName;
+        }
+        return firstName + " " + lastName;
+    }
+}
