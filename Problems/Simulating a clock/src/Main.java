a1f46b6de464548bd7d0bea9eadbef94d0740f60
Dzmitry Marudau <vinmaster@tut.by>
2020-08-09 16:16:46 +0300

    Solve tasks: - Simulating a clock

---
+++ Problems/Simulating a clock/src/Main.java
@@ -0,0 +1,18 @@
+class Clock {
+
+    int hours = 12;
+    int minutes = 0;
+
+
+    void next() {
+        minutes++;
+        if (minutes == 60) {
+            minutes = 0;
+            hours++;
+            if (hours >= 13) {
+                hours %= 12;
+            }
+        }
+    }
+
+}
