63ea865dbde634efe7a72eb56ff2e411eb51cf7d
Dzmitry Marudau <vinmaster@tut.by>
2020-08-27 19:50:46 +0300

    Solve tasks: - Composite numbers

--- Problems/Number parsing/src/Main.java
+++ Problems/Number parsing/src/Main.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            String regex = "[+-]?(0|[1-9]\\d*)([.,](0|\\d*[1-9]))?";
-            System.out.println(scanner.nextLine().matches(regex) ? "YES" : "NO");
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        String regex = "write-your-regex-here";
+
+        String number = scanner.nextLine();
+        System.out.println(number.matches(regex) ? "YES" : "NO");
     }
 }
