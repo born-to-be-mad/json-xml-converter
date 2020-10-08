d7866fa720a5b0a3c95be99e4f965dd23b709f42
Dzmitry Marudau <vinmaster@tut.by>
2020-10-08 19:26:35 +0300

    Solve tasks: - The sum of array elements

---
+++ Problems/The sum of array elements/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(
+            IntStream.range(0, scanner.nextInt())
+                     .mapToLong(value -> scanner.nextInt()).sum()
+        );
+    }
+}
