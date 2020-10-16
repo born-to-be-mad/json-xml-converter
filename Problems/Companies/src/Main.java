c1331e222f0fc1d8d2ca5700234f59ac78176ca1
Dzmitry Marudau <vinmaster@tut.by>
2020-10-16 14:18:03 +0300

    Solve tasks: - Companies

---
+++ Problems/Companies/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> list = Arrays.stream(scanner.nextLine().split(" "))
+                                  .collect(Collectors.toList());
+        System.out.println(list);
+    }
+}
