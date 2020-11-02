f30c9401f45ec5c87043e611e1dc5c6c2bfa689e
Dzmitry Marudau <vinmaster@tut.by>
2020-11-02 13:47:38 +0300

    Extend ArrayUtils

--- Problems/Check for null/src/Main.java
+++ Problems/Check for null/src/Main.java
@@ -7,4 +7,18 @@ class ArrayUtils {
     public static <T> boolean hasNull(T[] array) {
         return Arrays.stream(array).anyMatch(Objects::isNull);
     }
+
+    public static <T> T getFirst(T[] array) {
+        return Arrays.stream(array).findFirst().orElse(null);
+    }
+
+    public static <T> T[] invert(T[] array) {
+        int size = array.length;
+        for (int i = 0; i < size / 2; i++) {
+            T tmp = array[i];
+            array[i] = array[size - i - 1];
+            array[size - i - 1] = tmp;
+        }
+        return array;
+    }
 }
